1d920b4464588d01be77b1048b3f91b9b32144ae
Luiz Perren <luiz.perren@siga.swiss>
2025-05-14 15:38:03 +0200

    push

--- sigadesign/stencil.config.ts
+++ sigadesign/stencil.config.ts
@@ -7,27 +7,5 @@ export const config: Config = {
     { 
       type: 'dist', 
       esmLoaderPath: '../loader'
-    },
-    {
-      type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false
-    },
-    vueOutputTarget({
-      componentCorePackage: 'sigadesign',
-      proxiesFile: '../vue-app/src/components/stencil-components.ts',
-      includeDefineCustomElements: true,
-      loaderDir: '../loader'    
-    }),
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
-  ],
-  testing: {
-    browserHeadless: "shell",
-  },
+    }]
 };
