9c9f5140bda4994bcbe108bb9a6d6cc093f5893c
bernobin <berno.binkert@gmaile.com>
2025-05-28 16:12:12 +0200

    global vars second attempt

--- sigadesign/stencil.config.ts
+++ sigadesign/stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'sigadesign',
+  globalStyle: 'src/global/app.css',
   outputTargets: [
     { 
       type: 'dist', 
