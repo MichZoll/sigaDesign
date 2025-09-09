04cab455c888e678a51b0057e5a04e0d1d1283c2
bernobin <berno.binkert@gmaile.com>
2025-09-09 10:42:01 +0200

    new export

--- sigadesign/stencil.config.ts
+++ sigadesign/stencil.config.ts
@@ -9,6 +9,10 @@ export const config: Config = {
       esmLoaderPath: '../loader'
     },
     {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'bundle',
+    },
+    {
       type: 'www',
       serviceWorker: null,
       copy: [{ src: 'assets' }]
